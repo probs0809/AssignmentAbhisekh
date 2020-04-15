2154977eb45ad6f8a70beaf3866ee2b68e42e20e
Abhisekh Sahoo <abhisekhsahoo5@gmail.com>
2020-04-16 00:11:19 +0530

    1.Use implicit intent and open dialer and camera using intent action and show the clicked   image in image view in an android application.   2. Create a List view with custom adapter with 10 rows and add image views and text view   and make one relevant android application.   3. Develop one android application to create the database to save employee name and salary   using Firebase Connectivity.

---
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.content.Intent.ACTION_DIAL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int pic_id = 123;
+    Button camera_open_id;
+    ImageView click_image_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        camera_open_id = findViewById(R.id.camera_button);
+        click_image_id = findViewById(R.id.click_image);
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent camera_intent
+                        = new Intent(MediaStore
+                        .ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == pic_id) {
+
+            Bitmap photo = (Bitmap) data.getExtras()
+                    .get("data");
+            click_image_id.setImageBitmap(photo);
+        }
+    }
+
+    public void dial(View view) throws SecurityException {
+        Intent i = new Intent(ACTION_DIAL);
+        startActivity(i);
+    }
+}
